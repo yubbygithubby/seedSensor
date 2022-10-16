67ea7bca08e5327725ed7dc0c32e0df5d1f7bc18
Austin <78719539+yubbygithubby@users.noreply.github.com>
2022-10-16 15:14:10 +1000

    Add files via upload

--- testConnect.py
+++ testConnect.py
@@ -1,15 +1,19 @@
-from connectSense import *
+from unittest import result
 
+from numpy import recfromcsv
+from connectSense import *
+currentData = 'OFF'
 try:
     connectESP32()
     print("connected")
 except:
     print("can't connect")
 
-count =0
+count = 0
 
 while(count<500):
-    print(receiveData())
+    result = receiveData(currentData)
+    print(result)
     count+=1
 
 disconnectESP32()
